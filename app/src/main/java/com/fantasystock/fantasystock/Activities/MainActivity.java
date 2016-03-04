b99a1ef5fe970d1f5eed81acd6ff2abc92f1a324
WeiSheng Su <ted7726@gmail.com>
2016-03-03 22:29:06 -0800

    integrate with parse heroku

--- app/src/main/java/com/fantasystock/fantasystock/Activities/MainActivity.java
+++ app/src/main/java/com/fantasystock/fantasystock/Activities/MainActivity.java
@@ -1,15 +1,35 @@
 package com.fantasystock.fantasystock.Activities;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.fantasystock.fantasystock.R;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupParse();
+
+        ParseObject testObject = new ParseObject("TestingFantasyObject");
+        testObject.put("AAPL", "100");
+        testObject.saveInBackground();
+
+
+
         setContentView(R.layout.activity_main);
+    }
+
+    private void setupParse() {
+        Resources res = getResources();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(res.getString(R.string.parse_app_id)) // should correspond to APP_ID env variable
+                .clientKey(res.getString(R.string.parse_client_key))  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server(res.getString(R.string.parse_server_url)).build());
+
     }
 }
