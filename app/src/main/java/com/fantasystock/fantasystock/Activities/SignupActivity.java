1995d64ef516fbd056920bda9c451e225b9af2fa
sammsiontir <chengfu@umich.edu>
2016-03-04 01:52:10 -0800

    Change launcher activity to login page and integrate with facebook login

--- app/src/main/java/com/fantasystock/fantasystock/Activities/SignupActivity.java
+++ app/src/main/java/com/fantasystock/fantasystock/Activities/SignupActivity.java
@@ -1,17 +1,65 @@
 package com.fantasystock.fantasystock.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.fantasystock.fantasystock.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class SignupActivity extends AppCompatActivity {
+    private CallbackManager callbackManager;
+
+    @Bind(R.id.login_button) LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initial facebook sdk
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        // Set content and bind views
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        // Initial facebook callback manager
+        callbackManager = CallbackManager.Factory.create();
+        // Ask facebook permission if needed
+        // example:
+        // loginButton.setReadPermissions("user_friends");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent i = new Intent(getApplication(), MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "You must login", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(), "Login failed, please try again", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
