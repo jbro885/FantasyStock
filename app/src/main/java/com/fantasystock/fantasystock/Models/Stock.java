eee59b9775e314c1e1acf8038b778b10ce63fbfc
sammsiontir <chengfu@umich.edu>
2016-03-03 23:21:59 -0800

    Create data models

---
+++ app/src/main/java/com/fantasystock/fantasystock/Models/Stock.java
@@ -0,0 +1,46 @@
+package com.fantasystock.fantasystock.Models;
+
+/**
+ * Created by chengfu_lin on 3/3/16.
+ */
+public class Stock {
+    public String name;
+    public float price;
+}
+
+
+/*
+Fetch from google
+http://finance.google.com/finance/info?client=ig&q=AAPL
+[
+  {
+    "id": "22144",
+    "t": "AAPL",
+    "e": "NASDAQ",
+    "l": "101.50",
+    "l_fix": "101.50",
+    "l_cur": "101.50",
+    "s": "2",
+    "ltt": "4:00PM EST",
+    "lt": "Mar 3, 4:00PM EST",
+    "lt_dts": "2016-03-03T16:00:02Z",
+    "c": "+0.75",
+    "c_fix": "0.75",
+    "cp": "0.74",
+    "cp_fix": "0.74",
+    "ccol": "chg",
+    "pcls_fix": "100.75",
+    "el": "101.83",
+    "el_fix": "101.83",
+    "el_cur": "101.83",
+    "elt": "Mar 3, 7:59PM EST",
+    "ec": "+0.33",
+    "ec_fix": "0.33",
+    "ecp": "0.33",
+    "ecp_fix": "0.33",
+    "eccol": "chg",
+    "div": "0.52",
+    "yld": "2.05"
+  }
+]
+ */
