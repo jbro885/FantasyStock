65fbbb84efa5019585306b5434c87bfec2fada15
sammsiontir <chengfu@umich.edu>
2016-03-03 23:49:01 -0800

    Integrate with facebook SDK

--- app/src/main/java/com/fantasystock/fantasystock/Activities/SignupActivity.java
+++ app/src/main/java/com/fantasystock/fantasystock/Activities/SignupActivity.java
@@ -1,8 +1,9 @@
 package com.fantasystock.fantasystock.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.appevents.AppEventsLogger;
 import com.fantasystock.fantasystock.R;
 
 public class SignupActivity extends AppCompatActivity {
@@ -11,5 +12,20 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 }
