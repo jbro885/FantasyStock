bd644bf570c98831ec6ca1187e481f96d70c40c7
WeiSheng Su <ted7726@gmail.com>
2016-03-06 20:54:28 -0800

    polish charts

--- app/src/main/java/com/fantasystock/fantasystock/Fragments/ChartsFragment.java
+++ app/src/main/java/com/fantasystock/fantasystock/Fragments/ChartsFragment.java
@@ -13,8 +13,12 @@ import android.view.ViewGroup;
 import com.fantasystock.fantasystock.CallBack;
 import com.fantasystock.fantasystock.DataClient;
 import com.fantasystock.fantasystock.Models.HistoricalData;
+import com.fantasystock.fantasystock.Models.Stock;
 import com.fantasystock.fantasystock.R;
+import com.fantasystock.fantasystock.Utils;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -22,6 +26,7 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import butterknife.Bind;
@@ -34,6 +39,8 @@ public class ChartsFragment extends Fragment {
     @Bind(R.id.lcChart) LineChart lineChart;
     private DataClient client;
     private HistoricalData data;
+    private LimitLine openLimitLine;
+    public Stock stock;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,37 +73,58 @@ public class ChartsFragment extends Fragment {
         // mChart.setScaleXEnabled(true);
         // mChart.setScaleYEnabled(true);
         lineChart.getAxisRight().setEnabled(false);
-        lineChart.getAxisLeft().setGridColor(R.color.transparent);
 
+        lineChart.setDrawGridBackground(false);
+        lineChart.getXAxis().setDrawGridLines(false);
+        lineChart.getXAxis().setDrawAxisLine(false);
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        lineChart.getXAxis().setTextColor(R.color.grey);
+        lineChart.getAxisLeft().setDrawGridLines(false);
+        lineChart.getAxisLeft().setDrawAxisLine(false);
+        lineChart.getAxisLeft().setTextColor(R.color.grey);
 
         // if disabled, scaling can be done on x- and y-axis separately
         lineChart.setPinchZoom(true);
 
 
+
+
+
+
         client.getHistoricalPrices("YHOO", "1d", callBackHandler());
     }
+    public void setStock(Stock stock) {
+        this.stock = stock;
 
-    public void setData(HistoricalData data) {
+    }
+
+    private void setData(HistoricalData data) {
         this.data = data;
         ArrayList<Entry> yVals = new ArrayList<>();
         ArrayList<String> xVals = new ArrayList<>();
         List<HistoricalData.SeriesEntity> prices = data.series;
         int len = prices.size();
-
+        if (len>0) {
+            openLimitLine = new LimitLine(prices.get(0).open, "");
+            openLimitLine.setLineWidth(1.5f);
+            openLimitLine.setLineColor(Color.parseColor("#55ee0000"));
+            lineChart.getAxisLeft().addLimitLine(openLimitLine);
+        }
 
         for (int i=0;i<len; ++i) {
             HistoricalData.SeriesEntity series = prices.get(i);
             yVals.add(new Entry(series.close, i));
-            xVals.add(i+"a");
+            xVals.add("");
         }
-        LineDataSet lineDataSet= new LineDataSet(yVals, "dataSet 1");
-        lineDataSet.setColor(R.color.darkBlue);
-        lineDataSet.setCircleColor(R.color.darkBlue);
-        lineDataSet.setLineWidth(1f);
-        lineDataSet.setCircleRadius(1f);
+        int darkColor = Color.parseColor("#2cbcb6");
+        LineDataSet lineDataSet= new LineDataSet(yVals, "YHOO");
+
+        lineDataSet.setColor(darkColor);
+        lineDataSet.setCircleColor(darkColor);
+        lineDataSet.setLineWidth(1.5f);
+        lineDataSet.setCircleRadius(0.0f);
         lineDataSet.setDrawCircleHole(false);
         lineDataSet.setDrawCubic(true);
-        lineDataSet.setValueTextColor(R.color.grey);
         lineDataSet.setValueTextSize(9f);
         Drawable drawable = ContextCompat.getDrawable(getActivity(), R.drawable.fade_blue);
         lineDataSet.setFillDrawable(drawable);
@@ -106,7 +134,6 @@ public class ChartsFragment extends Fragment {
 
         // create a data object with the datasets
         LineData lineData = new LineData(xVals, dataSets);
-
         lineChart.setData(lineData);
         lineChart.animateX(1500);
         lineChart.invalidate();
