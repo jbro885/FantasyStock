5d58719d00ab24a0ffcfbe8f5ddc7ce2a968f063
WeiSheng Su <ted7726@gmail.com>
2016-03-08 02:48:53 -0800

    update indexes and repeating animation

--- app/src/main/java/com/fantasystock/fantasystock/Activities/MainActivity.java
+++ app/src/main/java/com/fantasystock/fantasystock/Activities/MainActivity.java
@@ -2,18 +2,28 @@ package com.fantasystock.fantasystock.Activities;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.fantasystock.fantasystock.CallBack;
 import com.fantasystock.fantasystock.DataCenter;
+import com.fantasystock.fantasystock.DataClient;
 import com.fantasystock.fantasystock.Fragments.MainListFragment;
+import com.fantasystock.fantasystock.Models.Stock;
 import com.fantasystock.fantasystock.R;
+import com.fantasystock.fantasystock.Utils;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -28,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.ivBackground) ImageView ivBackground;
     @Bind(R.id.ivBackgroundBlurred) ImageView ivBackgroundBlurred;
     @Bind(R.id.rlDim) RelativeLayout rlDim;
+    // Indexs
+    @Bind(R.id.tvIndexName) TextView tvIndexName;
+    @Bind(R.id.tvIndexPrice) TextView tvIndexPrice;
+    @Bind(R.id.tvIndexPriceChange) TextView tvIndexPriceChange;
+    @Bind(R.id.llIndexes) LinearLayout llIndexes;
+    private ArrayList<Stock> stocks;
+    private int indexesIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +65,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
                 handleScrolling(verticalOffset);
+            }
+        });
+
+        stocks = new ArrayList<>();
+        stocks.add(new Stock(".DJI"));
+        stocks.add(new Stock(".INX"));
+        stocks.add(new Stock(".IXIC"));
+
+        DataClient.getInstance().getStocksPrices(stocks, new CallBack() {
+            @Override
+            public void stocksCallBack(ArrayList<Stock> returnedStocks) {
+                stocks.clear();
+                stocks.addAll(returnedStocks);
+                stocks.get(0).name = "DOW";
+                stocks.get(1).name = "S&P500";
+                stocks.get(2).name = "NASDAQ";
+                indexesIndex = 0;
+                Utils.repeatAnimationGenerator(llIndexes, new CallBack() {
+                    @Override
+                    public void task() {
+                        Stock stock = stocks.get(indexesIndex++);
+                        tvIndexName.setText(stock.name);
+                        tvIndexPrice.setText(stock.current_price + "");
+                        tvIndexPriceChange.setText(stock.current_change + "(" + stock.current_change_percentage + "%)");
+                        if (Float.parseFloat(stock.current_change) < 0) {
+                            tvIndexPriceChange.setTextColor(Color.RED);
+                        } else {
+                            tvIndexPriceChange.setTextColor(Color.GREEN);
+                        }
+                        indexesIndex = indexesIndex % 3;
+                    }
+                });
             }
         });
 
@@ -78,14 +127,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void handleScrolling(int verticalOffset) {
-        float alpha = Math.abs(verticalOffset)/2500.0f;
+        float alpha = Math.abs(verticalOffset)/3000.0f;
         if (alpha > 1) {
             alpha = 1;
         }
-        ivBackgroundBlurred.setAlpha(1 - alpha*0.3f);
-        Log.d("DEBUG", verticalOffset + ", " + alpha);
-
-
-
+        ivBackgroundBlurred.setAlpha(1 - alpha * 0.3f);
     }
 }
--- app/src/main/java/com/fantasystock/fantasystock/Utils.java
+++ app/src/main/java/com/fantasystock/fantasystock/Utils.java
@@ -1,6 +1,9 @@
 package com.fantasystock.fantasystock;
 
 import android.text.format.DateUtils;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -28,5 +31,41 @@ public class Utils {
     public static String numberConverter(int n) {
         String text =  (n > 1000 ? n / 1000 + "k" : n + "");
         return text;
+    }
+
+    public static void repeatAnimationGenerator(final View view, final CallBack callBack) {
+        final AlphaAnimation animation1 = new AlphaAnimation(0.0f, 1.0f);
+        final AlphaAnimation animation2 = new AlphaAnimation(1.0f, 0.0f);
+        animation1.setDuration(500);
+        animation2.setDuration(500);
+        animation2.setStartOffset(5000);
+
+        //animation1 AnimationListener
+        animation1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                // start animation2 when animation1 ends (continue)
+                view.startAnimation(animation2);
+            }
+        });
+
+        //animation2 AnimationListener
+        animation2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                // start animation1 when animation2 ends (repeat)
+                callBack.task();
+                view.startAnimation(animation1);
+            }
+        });
+        view.startAnimation(animation1);
     }
 }
