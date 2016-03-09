40321a426b8cafe8202ed868537e4c2c2b3a4eee
Qi Zhu <qi.zhu@airbnb.com>
2016-03-09 01:51:01 -0800

    fix mege issue, again

--- app/src/main/java/com/fantasystock/fantasystock/Activities/DetailActivity.java
+++ app/src/main/java/com/fantasystock/fantasystock/Activities/DetailActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import com.fantasystock.fantasystock.DataCenter;
 import com.fantasystock.fantasystock.Fragments.DetailFragment;
@@ -26,13 +25,14 @@ import butterknife.OnClick;
 
 public class DetailActivity extends AppCompatActivity {
     @Bind(R.id.vpViewPager) ViewPager vpViewPager;
+    private String symbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
-        String symbol = intent.getStringExtra("symbol");
+        symbol = intent.getStringExtra("symbol");
 
         ButterKnife.bind(this);
         Drawable fadeBlue = ContextCompat.getDrawable(this, R.drawable.fade_blue);
@@ -53,15 +53,6 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    private static class DetailsPagerAdapter extends FragmentPagerAdapter {
-        private ArrayList<Stock> stocks;
-        private Drawable fadeBlue;
-        public DetailsPagerAdapter(FragmentManager fm, Drawable fadeBlue) {
-            super(fm);
-            this.fadeBlue = fadeBlue;
-            stocks = DataCenter.getInstance().allFavoritedStocks();
-        }
-
     @OnClick(R.id.btnBuy)
     public void onBuy(View view) {
         onTrade("buy");
@@ -79,23 +70,15 @@ public class DetailActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private CallBack profileCallbackHandler() {
-        return new CallBack(){
-            @Override
-            public void profileCallBack(Profile profile) {
-                tv52High.setText(profile.yr_high);
-                tv52Low.setText(profile.yr_low);
-                tvLow.setText(profile.low);
-                tvHigh.setText(profile.high);
-                tvPERatio.setText(profile.eps);
-                tvMarketCap.setText(profile.mkt_cap);
-                tvOpen.setText(profile.open);
-                tvDivYield.setText(profile.dividend_yld);
-                tvVolume.setText(profile.vol);
-                tvAvgVolume.setText(profile.ave_vol);
-            }
-        };
-    }
+    private static class DetailsPagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Stock> stocks;
+        private Drawable fadeBlue;
+        public DetailsPagerAdapter(FragmentManager fm, Drawable fadeBlue) {
+            super(fm);
+            this.fadeBlue = fadeBlue;
+            stocks = DataCenter.getInstance().allFavoritedStocks();
+        }
+
         @Override
         public Fragment getItem(int position) {
             DetailFragment detailFragment = DetailFragment.newInstance(stocks.get(position).symbol);
