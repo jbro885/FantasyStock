e3c614a063c4a73cb9d8ba0e2732ceb7bedc2453
WeiSheng Su <ted7726@gmail.com>
2016-03-16 17:45:32 -0700

    finish ranking

--- app/src/main/java/com/fantasystock/fantasystock/CallBack.java
+++ app/src/main/java/com/fantasystock/fantasystock/CallBack.java
@@ -6,8 +6,10 @@ import com.fantasystock.fantasystock.Models.News;
 import com.fantasystock.fantasystock.Models.Profile;
 import com.fantasystock.fantasystock.Models.Stock;
 import com.fantasystock.fantasystock.Models.Transaction;
+import com.fantasystock.fantasystock.Models.User;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wilsonsu on 3/5/16.
@@ -23,6 +25,7 @@ public class CallBack {
     public void transactionsCallBack(ArrayList<Transaction> transactions) {}
     public void commentsCallBack(ArrayList<Comment> comments) {}
     public void commentCallBack(Comment comment) {}
+    public void usersCallBack(ArrayList<User> users) {}
     public void onFail(String failureMessage){}
 
 
