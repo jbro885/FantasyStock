e017a4478506b680cdba179ff7ae126210e2b394
Qi Zhu <qi.zhu@airbnb.com>
2016-04-01 23:09:22 -0700

    show posted comments without refresh

--- app/src/main/java/com/fantasystock/fantasystock/Fragments/CommentsFullPageFragment.java
+++ app/src/main/java/com/fantasystock/fantasystock/Fragments/CommentsFullPageFragment.java
@@ -1,28 +1,17 @@
 package com.fantasystock.fantasystock.Fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.fantasystock.fantasystock.Helpers.CallBack;
 import com.fantasystock.fantasystock.Models.Comment;
-import com.fantasystock.fantasystock.Models.User;
 import com.fantasystock.fantasystock.R;
-import com.fantasystock.fantasystock.Helpers.Utils;
-import com.parse.ParseUser;
-
-import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -59,8 +48,9 @@ public class CommentsFullPageFragment extends CommentsFragment{
         Comment.addComment(etCommentText.getText().toString(), symbol, new CallBack() {
             @Override
             public void commentCallBack(Comment comment) {
-                comments.add(comment);
+                comments.add(0, comment);
                 adapter.notifyDataSetChanged();
+                adapter.notifyItemInserted(0);
                 etCommentText.setText("");
             }
         });
