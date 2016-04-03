c778aea71b24f257f9c0878d34d9b0ff68b98b48
sammsiontir <chengfu@umich.edu>
2016-04-03 05:08:32 -0700

    Add animation to open trade dialog

--- app/src/main/java/com/fantasystock/fantasystock/Fragments/TradeFragment.java
+++ app/src/main/java/com/fantasystock/fantasystock/Fragments/TradeFragment.java
@@ -1,6 +1,8 @@
 package com.fantasystock.fantasystock.Fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
@@ -65,6 +67,14 @@ public class TradeFragment extends DialogFragment {
         args.putBoolean("isbuy", isBuy);
         frag.setArguments(args);
         return frag;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.MyAnimation_Window;
+        return dialog;
     }
 
     @Override
